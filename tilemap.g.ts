9aa4a5c35c6acad9ab36c8e11c1213eed2f4f0ab
Isaiahmiron <122472442+Isaiahmiron@users.noreply.github.com>
2023-02-08 14:33:42 -0600

    Tile map and spawning

---
+++ tilemap.g.ts
@@ -0,0 +1,58 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`200010000709090909090909090909090909090e1114141414141414141414141414141901020303030303030303030303030c0f1206161616161616161616161616181a01030303030303030303030303030c0f1206161616161616161616161616161a01040404060606060404040303030c0f1206161616151515151515151515151a01040404040404040404040303030c0f1206161616151515151515151515151a01030305030303030303030303030c0f1206161616161616161616161616161a01030305030303030303030303030c0f1206161616161616161616161616161a01030305030303030303030303030c0f1206161616161616161616161616161a01030304040b0b04040606040404040f1215151515151515151515151516161a0103060404040404040404040404040f1215151515151515151515151516161a0103060303030303030303030303030f1216161616161616161616161616161a0103060303030303030303030303030f1216161616161616161616161616161a0103060303030406060606040303030f1216161616161616161616161616161a0103030303040404040404040403030f1216161616161616161616161616161a01030303040404040404040404040d0f1216161616161616161616161616161a080a0a0a0a0a0a0a0a0a0a0a0a0a0a101317171717171717171717171717171b`, img`
+22222222222222222222222222222222
+2..............22..............2
+2222....222....22..............2
+22222222222....22..............2
+22222222222....22..............2
+2..2...........22..............2
+2..2...........22..............2
+2..2...........22..............2
+2..22..22..222222..............2
+2..22222222222222..............2
+2..............22..............2
+2..............22..............2
+2.....2....2...22..............2
+2....22222222..22..............2
+2...2222222222.22..............2
+22222222222222222222222222222222
+`, [myTiles.transparency16,sprites.dungeon.greenOuterWest0,sprites.dungeon.stairWest,sprites.dungeon.greenInnerSouthWest,sprites.skillmap.islandTile4,sprites.builtin.brick,sprites.dungeon.hazardWater,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterSouth0,sprites.dungeon.hazardLava0,sprites.dungeon.hazardLava1,sprites.dungeon.stairSouth,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.floorLight2,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleOuterSouth0,sprites.dungeon.stairEast,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterSouthWest], TileScale.Sixteen);
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`20001000010202020202020202020202020202031316161616161616161616161616161208100909090909090909090909090c04170b1b1b1b1b1b1b1b1b1b1b1b1b111508090909090909090909090909090c04170b1b1b1b1b1b1b1b1b1b1b1b1b1b15080a0a0a0b0b0b0b0a0a0a0909090c04170b1b1b1b1a1a1a0d0d0d0d1a1a1a15080a0a0a0a0a0a0a0a0a0a0909090c04170b1b1b1b1a1a1a1a1a1a1a1a1a1a150809090e090909090909090909090c04170b1b1b1b1b1b1b1b1b1b1b0e1b1b150809090e090909090909090909090c04170b1b1b1b1b1b1b1b1b1b1b0e1b1b150809090e090909090909090909090c04170b1b1b1b1b1b1b1b1b1b1b0e1b1b150809090a0a0d0d0a0a0b0b0a0a0a0a04171a1a1a1a0d0d1a1a0b0b1a1a1b1b1508090b0a0a0a0a0a0a0a0a0a0a0a0a04171a1a1a1a1a1a1a1a1a1a1a1a0c1b1508090b09090909090909090909090904171b1b1b1b1b1b1b1b1b1b1b1b0c1b1508090b09090909090909090909090904171b1b1b1b1b1b1b1b1b1b1b1b0c1b1508090b0909090a0b0b0b0b0a09090904171b1b1b1a0d0d0d0d1a1b1b1b0c1b1508090909090a0a0a0a0a0a0a0a090904171b1b1a1a1a1a1a1a1a1a1b1b1b1b15080909090a0a0a0a0a0a0a0a0a0a0f04171c1a1a1a1a1a1a1a1a1a1a1b1b1b150607070707070707070707070707070514181818181818181818181818181819`, img`
+22222222222222222222222222222222
+2..............22..............2
+2..............22..............2
+2222....222....22....222....2222
+22222222222....22....22222222222
+2..2...........22...........2..2
+2..2...........22...........2..2
+2..2...........22...........2..2
+2..22..22..2222222222..22..22..2
+2..22222222222222222222222222..2
+2..............22..............2
+2..............22..............2
+2.....2....2...22...2....2.....2
+2....22222222..22..22222222....2
+2...2222222222.22.2222222222...2
+22222222222222222222222222222222
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenInnerSouthWest,sprites.skillmap.islandTile4,sprites.dungeon.hazardWater,sprites.dungeon.hazardLava1,sprites.dungeon.hazardLava0,sprites.builtin.brick,sprites.dungeon.stairSouth,sprites.dungeon.stairWest,sprites.dungeon.stairEast,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterSouth0,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.floorLight2,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.stairNorth], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
