53c0d6999d03a789aab17e6bcb590c7ef226ebd5
Isaiahmiron <122472442+Isaiahmiron@users.noreply.github.com>
2023-02-10 14:02:03 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
+let Level = 0
+let player2: Sprite = null
+let player1: Sprite = null
 function levelThree () {
+    Level = game.askForNumber("What level would you like to play? 1 2 or 3?", 1)
+    if (Level == 1) {
+        levelOne()
+    } else if (Level == 2) {
+        levelTwo()
+    } else if (Level == 3) {
+        levelThree()
+    } else if (Level == 999) {
+        tiles.setCurrentTilemap(tilemap`level4`)
+    }
     tiles.setCurrentTilemap(tilemap`level3`)
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -11,45 +24,8 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 function levelOne () {
     tiles.setCurrentTilemap(tilemap`level1`)
-    createPlayers()
     tiles.placeOnRandomTile(player1, sprites.dungeon.stairWest)
     tiles.placeOnRandomTile(player2, sprites.dungeon.stairEast)
-}
-function createPlayers () {
-    player1 = sprites.create(img`
-        . . . . . . . . . . b 5 b . . . 
-        . . . . . . . . . b 5 b . . . . 
-        . . . . . . . . . b c . . . . . 
-        . . . . . . b b b b b b . . . . 
-        . . . . . b b 5 5 5 5 5 b . . . 
-        . . . . b b 5 d 1 f 5 5 d f . . 
-        . . . . b 5 5 1 f f 5 d 4 c . . 
-        . . . . b 5 5 d f b d d 4 4 . . 
-        b d d d b b d 5 5 5 4 4 4 4 4 b 
-        b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
-        b d c 5 5 5 5 d 5 5 5 5 5 b . . 
-        c d d c d 5 5 b 5 5 5 5 5 5 b . 
-        c b d d c c b 5 5 5 5 5 5 5 b . 
-        . c d d d d d d 5 5 5 5 5 d b . 
-        . . c b d d d d d 5 5 5 b b . . 
-        . . . c c c c c c c c b b . . . 
-        `, SpriteKind.Player)
-    player2 = sprites.create(img`
-        e e e . . . . e e e . . . . 
-        c d d c . . c d d c . . . . 
-        c b d d f f d d b c . . . . 
-        c 3 b d d b d b 3 c . . . . 
-        f b 3 d d d d 3 b f . . . . 
-        e d d d d d d d d e . . . . 
-        e d f d d d d f d e . b f b 
-        f d d f d d f d d f . f d f 
-        f b d d b b d d 2 f . f d f 
-        . f 2 2 2 2 2 2 b b f f d f 
-        . f b d d d d d d b b d b f 
-        . f d d d d d b d d f f f . 
-        . f d f f f d f f d f . . . 
-        . f f . . f f . . f f . . . 
-        `, SpriteKind.Player)
 }
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.purpleSwitchUp, function (sprite, location) {
     tiles.setTileAt(tiles.getTileLocation(3, 5), sprites.dungeon.greenInnerSouthWest)
@@ -63,16 +39,4 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.purpleSwitchUp, function 
 })
 function levelTwo () {
     tiles.setCurrentTilemap(tilemap`level2`)
-}
-let player1: Sprite = null
-let player2: Sprite = null
-let Level = game.askForNumber("What level would you like to play? 1 2 or 3?", 1)
-if (Level == 1) {
-    levelOne()
-} else if (Level == 2) {
-    levelTwo()
-} else if (Level == 3) {
-    levelThree()
-} else if (Level == 999) {
-    tiles.setCurrentTilemap(tilemap`level4`)
 }
